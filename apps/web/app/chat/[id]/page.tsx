dc565c250c42198c15a53f439e8e0d7108db972e
Shreyas Jaltare <shreyasjaltare@gmail.com>
2025-02-01 23:35:11 +0530

    added http and ws servers. added route in web app

---
+++ apps/web/app/chat/[id]/page.tsx
@@ -0,0 +1,16 @@
+import { InputBox } from "@repo/ui/input-box";
+
+export default function(){
+    return(
+        <div>
+            <div>
+            chat room
+            </div>
+
+
+            <div>
+             <InputBox size="big" placeholder="enter text"></InputBox>   
+            </div>            
+        </div>
+    )
+}
